a4a06376150a005a815b798e6af1575ec83d6edd
sham <khaj3172@gmail.com>
2023-07-12 12:14:05 +0500

    modified:   package.json 	new file:   src/Api/client.jsx 	new file:   src/Api/server.jsx 	new file:   src/Components/spinner.jsx 	modified:   src/features/Posts/PostsSlice.jsx

--- src/features/Posts/PostsSlice.jsx
+++ src/features/Posts/PostsSlice.jsx
@@ -1,56 +1,73 @@
-import { createSlice , nanoid } from "@reduxjs/toolkit";
-import { sub } from 'date-fns'
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
-const initialState = [
-  { id: "1", title: "First Post!", content: "Hello!" , user : "0" , date: sub(new Date(), { minutes: 10 }).toISOString() , reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}},
-  { id: "2", title: "Second Post", content: "More text" , user : "1" ,date: sub(new Date(), { minutes: 5 }).toISOString() , reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0} },
-];
+const initialState = {
+  posts: [
+    {
+      id: "1",
+      title: "First Post!",
+      content: "Hello!",
+      user: "0",
+      date: sub(new Date(), { minutes: 10 }).toISOString(),
+      reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+    },
+    {
+      id: "2",
+      title: "Second Post",
+      content: "More text",
+      user: "1",
+      date: sub(new Date(), { minutes: 5 }).toISOString(),
+      reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+    },
+  ],
+  status: "idle",
+  error: null,
+};
 
 const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    addPost : {
-      reducer : (state , action) => {
-        state.push(action.payload)
+    addPost: {
+      reducer: (state, action) => {
+        state.posts.push(action.payload);
       },
-      prepare : (title , content, userId) => {
+      prepare: (title, content, userId) => {
         return {
-          payload : {
-            id : nanoid() ,
+          payload: {
+            id: nanoid(),
             date: new Date().toISOString(),
-            title ,
+            title,
             content,
-            user : userId,
-            reactions :{thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-          }
-        }
-      }
+            user: userId,
+            reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
+          },
+        };
+      },
     },
     reactionAdded(state, action) {
-      const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const { postId, reaction } = action.payload;
+      const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
-        existingPost.reactions[reaction]++
+        existingPost.reactions[reaction]++;
       }
     },
     postUpdated(state, action) {
-      const { id, title, content } = action.payload
-      const existingPost = state.find(post => post.id === id)
+      const { id, title, content } = action.payload;
+      const existingPost = state.posts.find((post) => post.id === id);
       if (existingPost) {
-        existingPost.title = title
-        existingPost.content = content
+        existingPost.title = title;
+        existingPost.content = content;
       }
-    }
+    },
   },
 });
 
-export const { addPost , postUpdated , reactionAdded } = postsSlice.actions
+export const { addPost, postUpdated, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
 
-
-export const selectAllPosts = state => state.posts
+export const selectAllPosts = (state) => state.posts.posts;
 
 export const selectPostById = (state, postId) =>
-  state.posts.find(post => post.id === postId)
+  state.posts.posts.find((post) => post.id === postId);
